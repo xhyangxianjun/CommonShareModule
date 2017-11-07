c689daf05b129276e66b69ccf3c77461cc85e516
ctddjyds <ctddjyds@163.com>
2017-11-07 17:23:29 +0800

    create first

---
+++ CTDDJYDS.DatabaseCommon/DatabaseCommon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CTDDJYDS.DatabaseCommon
+{
+    public enum DatabasePlatformType
+    {
+        Unknown = 0,
+        MySQL,
+        Firebird,
+        SQLServer2008,
+        SQLServer2012,
+        SQLServer2014,
+        SQLServer2016,
+        Oracle,
+    }
+    public class DatabaseCommon
+    {
+    }
+}
---
+++ CTDDJYDS.DatabaseCommon/IDBPlatformCommon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CTDDJYDS.DatabaseCommon
+{
+    interface IDBPlatformCommon
+    {
+        string _Server { get; }
+
+        uint _Port { get; }
+
+        string _User { get; }
+
+        string _Password { get; }
+
+        string _Database { get; }
+
+        bool _TrustedConnection { get; }
+
+        string _ErrorMessage { get; }
+
+        DatabasePlatformType DBPlatformType { get; }
+
+        System.ComponentModel.BackgroundWorker Worker { get; set; }
+
+        bool NHConnect(out string strException);
+
+        bool ConnectServer(string server, uint port, string user, string pwd, bool trustedConnection, out string[] databases);
+
+        bool ConnectServer(string server, uint port, string user, string pwd, bool trustedConnection);
+
+        bool IsDatabaseAccessible(string server, uint port, string user, string pwd, bool trustedConnection, string database);
+
+        void TryAutoStartDatabaseService();
+
+        bool CreateDatabase(string database);
+
+        bool CreateDatabase(string database, string dataPathForDB);
+
+        void RestoreConnection();
+
+        void AcceptConfig(string server, uint port, string user, string password, string database, bool trustedConnection = true);
+
+        bool GetDatabaseInfo(string dbName, out string createTime, out string lastAccessTime);
+    }
+}
